2505ef1545982cdf27dd15b64f457de74fc04cb1
Sebastien Jodogne <s.jodogne@gmail.com>
2012-10-26 14:25:29 +0200

    Orthanc-0.2.3

---
+++ Core/HttpServer/HttpOutput.h
@@ -0,0 +1,91 @@
+/**
+ * Orthanc - A Lightweight, RESTful DICOM Store
+ * Copyright (C) 2012 Medical Physics Department, CHU of Liege,
+ * Belgium
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * In addition, as a special exception, the copyright holders of this
+ * program give permission to link the code of its release with the
+ * OpenSSL project's "OpenSSL" library (or with modified versions of it
+ * that use the same license as the "OpenSSL" library), and distribute
+ * the linked executables. You must obey the GNU General Public License
+ * in all respects for all of the code used other than "OpenSSL". If you
+ * modify file(s) with this exception, you may extend this exception to
+ * your version of the file(s), but you are not obligated to do so. If
+ * you do not wish to do so, delete this exception statement from your
+ * version. If you delete this exception statement from all source files
+ * in the program, then also delete it here.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+
+#pragma once
+
+#include <string>
+#include <stdint.h>
+#include "../Enumerations.h"
+#include "../FileStorage.h"
+
+namespace Orthanc
+{
+  class HttpOutput
+  {
+  private:
+    void SendHeaderInternal(Orthanc_HttpStatus status);
+
+    void SendOkHeader(const char* contentType,
+                      bool hasContentLength,
+                      uint64_t contentLength,
+                      const char* contentFilename);
+
+  public:
+    virtual ~HttpOutput()
+    {
+    }
+
+    virtual void Send(const void* buffer, size_t length) = 0;
+
+    void SendOkHeader(const std::string& contentType);
+
+    void SendString(const std::string& s);
+
+    void SendMethodNotAllowedError(const std::string& allowed);
+
+    void SendHeader(Orthanc_HttpStatus status);
+
+
+    // Higher-level constructs to send entire files or buffers -------------------
+
+    void AnswerBufferWithContentType(const std::string& buffer,
+                                     const std::string& contentType);
+
+    void AnswerBufferWithContentType(const void* buffer,
+                                     size_t size,
+                                     const std::string& contentType);
+
+    void AnswerFileWithContentType(const std::string& path,
+                                   const std::string& contentType,
+                                   const char* filename = NULL);
+
+    void AnswerFileAutodetectContentType(const std::string& path,
+                                         const char* filename = NULL); 
+
+    void AnswerFile(const FileStorage& storage,
+                    const std::string& uuid,
+                    const std::string& contentType,
+                    const char* filename = NULL);
+
+    void Redirect(const std::string& path);
+  };
+}
---
+++ Resources/CMake/JsonCppConfiguration.cmake
@@ -0,0 +1,26 @@
+if (USE_DYNAMIC_JSONCPP)
+  CHECK_INCLUDE_FILE_CXX(jsoncpp/json/reader.h HAVE_JSONCPP_H)
+  if (NOT HAVE_JSONCPP_H)
+    message(FATAL_ERROR "Please install the libjsoncpp-dev package")
+  endif()
+
+  include_directories(/usr/include/jsoncpp)
+  link_libraries(jsoncpp)
+
+else()
+  SET(JSONCPP_SOURCES_DIR ${CMAKE_BINARY_DIR}/jsoncpp-src-0.5.0)
+  DownloadPackage("http://downloads.sourceforge.net/project/jsoncpp/jsoncpp/0.5.0/jsoncpp-src-0.5.0.tar.gz" "${JSONCPP_SOURCES_DIR}" "" "")
+
+  list(APPEND THIRD_PARTY_SOURCES
+    ${JSONCPP_SOURCES_DIR}/src/lib_json/json_reader.cpp
+    ${JSONCPP_SOURCES_DIR}/src/lib_json/json_value.cpp
+    ${JSONCPP_SOURCES_DIR}/src/lib_json/json_writer.cpp
+    )
+
+  include_directories(
+    ${JSONCPP_SOURCES_DIR}/include
+    )
+
+  source_group(ThirdParty\\JsonCpp REGULAR_EXPRESSION ${JSONCPP_SOURCES_DIR}/.*)
+endif()
+
